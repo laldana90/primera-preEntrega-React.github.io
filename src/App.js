875911a26afc49176cecefdcffd389bb2cd61e12
laldana <lar.90.rnr@gmail.com>
2023-02-12 16:58:53 -0600

    Carga Final

--- src/App.js
+++ src/App.js
@@ -1,28 +1,35 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css';
 import NavBar from './Components/NavBar';
 import ItemListContainer from './Components/ItemListContainer';
-import Cover from './Components/Cover.jsx'
-import Categories from './Components/Categories.jsx';
+import ItemListContainerCategorie from './Components/ItemListContainerCategorie';
+import ItemDetailContainer from './Components/ItemDetailContainer';
+/* import Cover from './Components/Cover.jsx'
+import Categories from './Components/Categories.jsx'; */
+
+
 import './Components/Styles/NavBar.css';
 import './Components/Styles/CartWidget.css';
 import './Components/Styles/ItemListContainer.css';
 import './Components/Styles/Categories.css';
 import './Components/Styles/Cover.css';
-
-
-
-
+import './Components/Styles/Counter.css';
+import './Components/Styles/ItemDetailContainer.css';
 
 function App() {
   return (
     <>
-    <NavBar/>
-    <Cover/>
-    <ItemListContainer greeting="Nuestras Categorias"/>
-    <Categories/>
-   
-    
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<NavBar/>}/>
+          <Route path='/categories/' element={<ItemListContainer greeting="Nuestras Categorias"/>}/>
+          <Route path='/category/:id' element={<ItemListContainerCategorie/>}/>
+          <Route path='/item/:id' element={<ItemDetailContainer/>}/>
+        </Routes>
+      </BrowserRouter>
+    </div>
     </>
 
   );
--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -5,22 +5,25 @@ import Navbar from 'react-bootstrap/Navbar';
 import "bootstrap/dist/css/bootstrap.min.css";
 import Logo from '../imgs/logo.png';
 import CartWidget from './CartWidget';
+import Carousel from 'react-bootstrap/Carousel';
+import Img1 from '../imgs/techstoreimg.jpg';
+import Img4 from '../imgs/techstoreimg4.png';
+import Img5 from '../imgs/techstoreimg5.jpg';
+import { Link } from "react-router-dom";
 
 
-
-
-
-function Navegador() {
+function NavBar() {
   return (
+    <>
     <header>
     <Navbar className="navi" bg="light" expand="lg">
       <Container>
-      <img className="logo" src={Logo} alt=""/>
+      <Link to={'/'} className="logo"><img src={Logo} alt=""/></Link>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#inicio">Inicio</Nav.Link>
-            <Nav.Link href="#categorias">Categorias</Nav.Link>
+            <Nav.Link href='/'>Inicio</Nav.Link>
+            <Nav.Link href='/categories/'>Categorias</Nav.Link>
             <Nav.Link href="#contacto">Contacto</Nav.Link>
             <Nav.Link href="#acercaDe">Acerca De</Nav.Link>
             </Nav>
@@ -31,11 +34,52 @@ function Navegador() {
     <div>
       <CartWidget/>
     </div>
-
-
-
     </header>
+
+
+    <Carousel className='carousel'>
+      <Carousel.Item>
+        <img
+          className="d-block w-100 carrousel-img"
+          src={Img1}
+          alt="First slide"
+        />
+        <Carousel.Caption className='carrousel-text'>
+          <h3>Bienvenido a X-tore</h3>
+          <p>A la vanguardia de la tecnologia para tu hogar.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+
+      <Carousel.Item>
+        <img
+          className="d-block w-100 carrousel-img"
+          src={Img4}
+          alt="Second slide"
+        />
+
+        <Carousel.Caption className='carrousel-text'>
+          <h3>Feria Samsung</h3>
+          <p>2 Samsung Galaxy al precio de uno.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+
+      <Carousel.Item>
+        <img
+          className="d-block w-100 carrousel-img"
+          src={Img5}
+          alt="Third slide"
+        />
+
+        <Carousel.Caption className='carrousel-text'>
+          <h3>Vivimos en Estado Play</h3>
+          <p>
+            No olvides visitar nuestra area de Videojuegos.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+    </>
   );
 }
 
-export default Navegador;
+export default NavBar;
